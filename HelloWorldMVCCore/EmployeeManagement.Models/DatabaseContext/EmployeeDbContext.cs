32cd44e333b85ef0fa9a82ab045bb0bf081f0149
shraktim <shahadathossainraktim@gmail.com>
2018-11-24 18:36:54 +0600

    migration add

--- HelloWorldMVCCore/EmployeeManagement.Models/DatabaseContext/EmployeeDbContext.cs
+++ HelloWorldMVCCore/EmployeeManagement.Models/DatabaseContext/EmployeeDbContext.cs
@@ -22,7 +22,7 @@ namespace EmployeeManagement.Models.EntityModels
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=(local);Database=EmployeeDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=PENCILBOX;Database=EmployeeDB;Trusted_Connection=True;");
             }
         }
 
